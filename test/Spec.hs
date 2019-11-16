53921bc3a47f4cf3dea3a2c5d5260243439fc7f0
grant weyburne <gbwey9@gmail.com>
2019-11-16 14:14:13 -0500

    fix tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -12,12 +12,12 @@ main :: IO ()
 main = suite
 
 suite :: IO ()
-suite = defaultMain $ testGroup "Spec"
-  [ testCase "typelevel.nats" $ (@?=) (getNats @'[2,4,3]) [2,4,3]
-  , testCase "typelevel.strings" $ (@?=) (getStrings @'["ab","c","def"]) ["ab","c","def"]
-  , testCase "typelevel.nats.enum.1" $ (@?=) (getNats @(EnumFromTo 12 15)) [12,13,14,15]
-  , testCase "typelevel.nats.enum.2" $ (@?=) (getNats @(EnumFromTo 12 100)) [12..100]
-  , testCase "typelevel.nats.mapfst" $ (@?=) (getNats @(Map FstSym0 '[ '(1,"ss"), '(77,"ddd"), '(200,"last") ])) [1,77,200]
-  , testCase "typelevel.strings.mapsnd" $ (@?=) (getStrings @(Map SndSym0 '[ '(1,"ss"), '(77,"ddd"), '(200,"last") ])) ["ss","ddd","last"]
-  , testCase "typelevel.natval" $ (@?=) (natVal (Proxy @(SUnWrap (FoldMap (SSumSym0 :.: FstSym0) '[ '(1,"ss"), '(77,"ddd"), '(200,"last") ])))) 278
+suite = defaultMain $ testGroup "TypeLevel"
+  [ testCase "nats" $ (@?=) (getNats @'[2,4,3]) [2,4,3]
+  , testCase "strings" $ (@?=) (getStrings @'["ab","c","def"]) ["ab","c","def"]
+  , testCase "nats.enum.1" $ (@?=) (getNats @(EnumFromTo 12 15)) [12,13,14,15]
+  , testCase "nats.enum.2" $ (@?=) (getNats @(EnumFromTo 12 100)) [12..100]
+  , testCase "nats.mapfst" $ (@?=) (getNats @(Map FstSym0 '[ '(1,"ss"), '(77,"ddd"), '(200,"last") ])) [1,77,200]
+  , testCase "strings.mapsnd" $ (@?=) (getStrings @(Map SndSym0 '[ '(1,"ss"), '(77,"ddd"), '(200,"last") ])) ["ss","ddd","last"]
+  , testCase "natval" $ (@?=) (natVal (Proxy @(SUnWrap (FoldMap (SSumSym0 :.: FstSym0) '[ '(1,"ss"), '(77,"ddd"), '(200,"last") ])))) 278
   ]
